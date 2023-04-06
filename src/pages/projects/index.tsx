53573dca0874095cd1776ebeae6e8c91bfd17eef
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2023-04-06 09:37:05 +0300

    Change CSS in projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -8,14 +8,14 @@ const Projects = () => {
     return (
         <>
         <Head>
-        <title>Projects</title>
-      </Head>
+          <title>Projects</title>
+        </Head>
         <div className={styles["wrapper"]}>
-           <div className={styles["container"]}>
+          <div className={styles["container"]}>
             <Link href="/projects/calculator" className={styles["link"]}>
             <Heading text="Calculator" tag="h2"/>
             <Image
-            className={styles["image"]}
+              className={styles["image"]}
               priority
               src="/calculator.png"
               height={150}
@@ -24,20 +24,20 @@ const Projects = () => {
             />
             </Link>
             <Link href="/projects/flash-card" className={styles["link"]}>
-            <Heading text="Flash card" tag="h2"/>
-            <Image
-            className={styles["image"]}
-              priority
-              src="/flashCard.png"
-              height={150}
-              width={150}
-              alt="Flash card"
-            />
+              <Heading text="Flash card" tag="h2"/>
+              <Image
+                className={styles["image"]}
+                priority
+                src="/flashCard.png"
+                height={150}
+                width={150}
+                alt="Flash card"
+              />
             </Link>
             <div className={styles["link"]}>
-            <Heading text="The rest will appear soon..." tag="h3"/>
+              <Heading text="The rest will appear soon..." tag="h3"/>
             </div>
-           </div>
+          </div>
         </div>
         </>
     );
