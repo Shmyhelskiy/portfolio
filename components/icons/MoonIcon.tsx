995da041debe597735d21d77fa90e4f177f58ef9
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2023-04-22 22:49:25 +0300

    test

--- components/icons/MoonIcon.tsx
+++ components/icons/MoonIcon.tsx
@@ -4,7 +4,6 @@ type MoonIconType = {
     className: string,
 }
 
-
 const MoonIcon:FC<IconsType> = ({className}) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${className}`}>
